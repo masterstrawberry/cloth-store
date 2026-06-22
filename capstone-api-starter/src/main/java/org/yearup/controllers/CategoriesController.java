538aa74e386ce58448924d7da3f3f4ac0e5f085a
masterstrawberry <jvillacis@my.yearupunited.org>
2026-06-22 19:19:09 -0400

    Initial commit

---
+++ capstone-api-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,107 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+import org.yearup.service.CategoryService;
+import org.yearup.service.ProductService;
+
+import java.util.List;
+
+// add the annotations to make this a REST controller
+// add the annotation to make this controller the endpoint for the following url
+    // http://localhost:8080/categories
+// add annotation to allow cross site origin requests
+@RestController
+@CrossOrigin
+@RequestMapping("/categories")
+public class CategoriesController
+{
+    private CategoryService categoryService;
+    private ProductService productService;
+
+
+    // create an Autowired constructor to inject the categoryService and productService
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping()
+    public List<Category> getAll()
+    {
+        // find and return all categories
+        return categoryService.getAllCategories();
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    public Category getById(@PathVariable int id)
+    {
+        // get the category by id
+        Category category = categoryService.getById(id);
+
+        if (category== null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return category;
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId)
+    {
+        // get a list of product by categoryId
+        return productService.listByCategoryId(categoryId);
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping()
+    public ResponseEntity<Category> addCategory(@RequestBody Category category)
+    {
+        // insert the category and return it with status 201 Created
+        Category savedCategory = categoryService.create(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("{id}")
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category)
+    {
+        // update the category by id and return the updated category (200 OK)
+
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        Category updatedCategory = categoryService.update(id, category);
+        return ResponseEntity.ok(updatedCategory).getBody();
+//        return categoryService.update(id, category);
+
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id)
+    {
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+
+    }
+}
---
+++ capstone-api-starter/src/main/java/org/yearup/service/CategoryService.java
@@ -0,0 +1,52 @@
+package org.yearup.service;
+
+import org.springframework.stereotype.Service;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+import org.yearup.repository.CategoryRepository;
+
+import java.util.List;
+
+@Service
+public class CategoryService
+{
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository)
+    {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories()
+    {
+        // get all categories
+         return categoryRepository.findAll();
+    }
+
+    public Category getById(int categoryId)
+    {
+        // get category by id
+        return categoryRepository.findById(categoryId).orElse(null);    }
+
+    public Category create(Category category)
+    {
+        // create a new category
+        return categoryRepository.save(category);
+    }
+
+    public Category update(int categoryId, Category category)
+    {
+        // update category and return the updated category
+        Category existing = categoryRepository.findById(categoryId).orElseThrow();
+        existing.setName(category.getName());
+        existing.setCategoryId(category.getCategoryId());
+        existing.setDescription(category.getDescription());
+
+        return categoryRepository.save(existing);
+    }
+
+    public void delete(int categoryId)
+    {
+        categoryRepository.deleteById(categoryId);
+    }
+}
