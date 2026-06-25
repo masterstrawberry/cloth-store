36a1957989bc23e34ed7a5e511fd135e4455e0a9
masterstrawberry <jvillacis@my.yearupunited.org>
2026-06-25 18:43:41 -0400

    finished phase 3, remember to run test 0 first

--- capstone-api-starter/src/main/java/org/yearup/service/ShoppingCartService.java
+++ capstone-api-starter/src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,13 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
 
 @Service
 public class ShoppingCartService
@@ -20,8 +25,55 @@ public class ShoppingCartService
     public ShoppingCart getByUserId(int userId)
     {
         // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        ShoppingCart shoppingCart = new ShoppingCart();
+        List<CartItem> CartItems = shoppingCartRepository.findByUserId(userId);
+
+        for (CartItem cartItem : CartItems) {
+            Product product = productService.getById(cartItem.getProductId());
+            ShoppingCartItem item = new ShoppingCartItem();
+            item.setProduct(product);
+            item.setQuantity(cartItem.getQuantity());
+            shoppingCart.add(item);
+        }
+
+//        items.stream().map(ShoppingItem ->{
+//            ShoppingCartItem item = new ShoppingCartItem();
+//            item.setProduct(productService.getById(ShoppingItem.getProductId()));
+//            item.setQuantity(ShoppingItem.getQuantity());
+//            return item;
+//        }).forEach(shoppingCart::add);
+        return shoppingCart;
     }
 
     // add additional methods here
+    public ShoppingCart addProductToCart(int userId,int productId){
+        CartItem cartItem = shoppingCartRepository.findByUserIdAndProductId(userId,productId);
+        if (cartItem == null){
+            CartItem newCartItem = new CartItem();
+            newCartItem.setUserId(userId);
+            newCartItem.setProductId(productId);
+            newCartItem.setQuantity(1);
+            shoppingCartRepository.save(newCartItem);
+        }else {
+            cartItem.setQuantity(cartItem.getQuantity()+1);
+            shoppingCartRepository.save(cartItem);
+        }
+        return getByUserId(userId);
+
+    }
+
+    public ShoppingCart editProduct(int userId,int productId, int quantity){
+        CartItem cartItem = shoppingCartRepository.findByUserIdAndProductId(userId,productId);
+        if (cartItem != null){
+            cartItem.setQuantity(quantity);
+            shoppingCartRepository.save(cartItem);
+        }
+
+        return getByUserId(userId);
+
+    }
+
+    public void clearCart(int userId){
+        shoppingCartRepository.deleteByUserId(userId);
+    }
 }
